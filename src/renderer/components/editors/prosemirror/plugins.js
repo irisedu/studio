33f01284fe80848e8abe399a2895db932fa5b703
voidedWarranties <hi+github@voidedxd.xyz>
2024-07-29 19:24:08 -0700

    ProseMirror: drop cursor
    Known issue: drop does nothing when the sidebar is open. Future versions of
    `react-arborist` may fix this.

--- src/renderer/components/editors/prosemirror/plugins.js
+++ src/renderer/components/editors/prosemirror/plugins.js
@@ -4,9 +4,19 @@ import { baseKeymap, docKeymap } from './keymap.js';
 import { inputRules } from 'prosemirror-inputrules';
 import { baseRules, docRules } from './inputrules.js';
 import { gapCursor } from 'prosemirror-gapcursor';
+import { dropCursor } from 'prosemirror-dropcursor';
 import { tableEditing, columnResizing } from 'prosemirror-tables';
 
-const _basePlugins = [history(), gapCursor(), columnResizing(), tableEditing()];
+const _basePlugins = [
+	history(),
+	dropCursor({
+		class: 'ProseMirror-dropcursor',
+		color: false
+	}),
+	gapCursor(),
+	columnResizing(),
+	tableEditing()
+];
 
 export const basePlugins = [
 	..._basePlugins,
